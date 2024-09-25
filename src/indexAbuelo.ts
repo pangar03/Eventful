19d6bac17de0b185419e141470abce6efe73f8be
Pangar03 <plopez0428@hotmail.com>
2024-09-25 11:23:25 -0500

    test: commented event details page test

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -16,20 +16,24 @@ class AppContainer extends HTMLElement {
                 <link rel="stylesheet" href="/src/indexAbuelo.css">`;
         }
 
-        const container = this.ownerDocument.createElement('main');
-        const eventPageDetails = this.ownerDocument.createElement('event-page-details') as EventPageDetails;
-        eventPageDetails.setAttribute(Attribute.eventtitle, 'Event Title');
-        eventPageDetails.setAttribute(Attribute.image, 'https://images.pexels.com/photos/587741/pexels-photo-587741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
-        eventPageDetails.setAttribute(Attribute.location, 'Event Location');
-        eventPageDetails.setAttribute(Attribute.date, 'Event Date');
-        eventPageDetails.setAttribute(Attribute.creator, 'Event Creator');
-        eventPageDetails.setAttribute(Attribute.attendants, String(3));
-        eventPageDetails.setAttribute(Attribute.maxattendants, String(15));
-        eventPageDetails.setAttribute(Attribute.description, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
-        eventPageDetails.setAttribute(Attribute.isattending, String(true));
+        // BEGIN TESTING
 
-        container.appendChild(eventPageDetails);
-        this.shadowRoot?.appendChild(container);
+        // TEST EVENT-PAGE-DETAILS
+
+        // const container = this.ownerDocument.createElement('main');
+        // const eventPageDetails = this.ownerDocument.createElement('event-page-details') as EventPageDetails;
+        // eventPageDetails.setAttribute(Attribute.eventtitle, 'Event Title');
+        // eventPageDetails.setAttribute(Attribute.image, 'https://images.pexels.com/photos/587741/pexels-photo-587741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+        // eventPageDetails.setAttribute(Attribute.location, 'Event Location');
+        // eventPageDetails.setAttribute(Attribute.date, 'Event Date');
+        // eventPageDetails.setAttribute(Attribute.creator, 'Event Creator');
+        // eventPageDetails.setAttribute(Attribute.attendants, String(3));
+        // eventPageDetails.setAttribute(Attribute.maxattendants, String(15));
+        // eventPageDetails.setAttribute(Attribute.description, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+        // eventPageDetails.setAttribute(Attribute.isattending, String(true));
+
+        // container.appendChild(eventPageDetails);
+        // this.shadowRoot?.appendChild(container);
     }
 }
 
