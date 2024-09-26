423e688e64afb8f5e74bd78075c2b6132953ee3a
Pangar03 <plopez0428@hotmail.com>
2024-09-26 16:59:59 -0500

    fix: adapt the data to be more adequate

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,145 +2,146 @@ export const posts = [
     {
         uid: 1,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1678703870962-166fe3f1d274?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGVyZmlsJTIwZGUlMjBob21icmV8ZW58MHx8MHx8fDA%3D",
         username: "Maurice K.",
-        posttext:
+        postText:
             "First and last time that I go to an Italian restaurant with white shirt lol... Rookie mistake tbf. At least I got to eat nice lasagna",
-        postimg: "",
+        postImg: "",
         likes: 100,
     },
     {
         uid: 2,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://vivolabs.es/wp-content/uploads/2022/03/perfil-mujer-vivo.png",
         username: "Laura S.",
-        posttext:
+        postText:
             "El mejor café que he probado en años. ¡Definitivamente volveré!",
-        postimg:
+        postImg:
             "https://plus.unsplash.com/premium_photo-1678901234567-abcdef654321?fm=jpg&q=60&w=3000",
         likes: 75,
     },
     {
         uid: 3,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://media.istockphoto.com/id/1200677760/es/foto/retrato-de-apuesto-joven-sonriente-con-los-brazos-cruzados.jpg?s=612x612&w=0&k=20&c=RhKR8pxX3y_YVe5CjrRnTcNFEGDryD2FVOcUT_w3m4w=",
         username: "Javier M.",
-        posttext:
+        postText:
             "La experiencia fue increíble, pero la comida no estuvo a la altura.",
-        postimg: "",
+        postImg: "",
         likes: 50,
     },
     {
         uid: 4,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-ghijkl123456?fm=jpg&q=60&w=3000",
         username: "Sofia R.",
-        posttext: "Un lugar mágico, perfecto para una cena romántica.",
-        postimg:
+        postText: "Un lugar mágico, perfecto para una cena romántica.",
+        postImg:
             "https://plus.unsplash.com/premium_photo-1688901234567-ghijkl654321?fm=jpg&q=60&w=3000",
         likes: 120,
     },
     {
         uid: 5,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-mnopqr123456?fm=jpg&q=60&w=3000",
         username: "Carlos T.",
-        posttext: "No me gustó el servicio, pero la comida estaba bien.",
-        postimg: "",
+        postText: "No me gustó el servicio, pero la comida estaba bien.",
+        postImg: "",
         likes: 30,
     },
     {
         uid: 6,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-stuvwx123456?fm=jpg&q=60&w=3000",
         username: "Ana P.",
-        posttext: "¡El mejor evento al que he asistido! Todo fue perfecto.",
-        postimg:
+        postText: "¡El mejor evento al que he asistido! Todo fue perfecto.",
+        postImg:
             "https://plus.unsplash.com/premium_photo-1688901234567-stuvwx654321?fm=jpg&q=60&w=3000",
         likes: 200,
     },
     {
         uid: 7,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-yzabcd123456?fm=jpg&q=60&w=3000",
         username: "Luis G.",
-        posttext: "No vale la pena, hay mejores opciones en la ciudad.",
-        postimg: "",
+        postText: "No vale la pena, hay mejores opciones en la ciudad.",
+        postImg: "",
         likes: 10,
     },
     {
         uid: 8,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-efghij123456?fm=jpg&q=60&w=3000",
         username: "Clara J.",
-        posttext: "Ambiente acogedor y buena música. Me encantó.",
-        postimg:
+        postText: "Ambiente acogedor y buena música. Me encantó.",
+        postImg:
             "https://plus.unsplash.com/premium_photo-1688901234567-efghij654321?fm=jpg&q=60&w=3000",
         likes: 90,
     },
     {
         uid: 9,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-klmnop123456?fm=jpg&q=60&w=3000",
         username: "Ricardo L.",
-        posttext: "Simplemente maravilloso. La atención fue excepcional.",
-        postimg: "",
+        postText: "Simplemente maravilloso. La atención fue excepcional.",
+        postImg: "",
         likes: 150,
     },
     {
         uid: 10,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-qrstuv123456?fm=jpg&q=60&w=3000",
         username: "Marta F.",
-        posttext: "La comida estaba fría y el lugar era ruidoso.",
-        postimg: "",
+        postText: "La comida estaba fría y el lugar era ruidoso.",
+        postImg: "",
         likes: 5,
     },
     {
         uid: 11,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-wxyzab123456?fm=jpg&q=60&w=3000",
         username: "Fernando H.",
-        posttext: "Un lugar ideal para familias. Muy divertido.",
-        postimg:
+        postText: "Un lugar ideal para familias. Muy divertido.",
+        postImg:
             "https://plus.unsplash.com/premium_photo-1688901234567-wxyzab654321?fm=jpg&q=60&w=3000",
         likes: 110,
     },
     {
         uid: 12,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-cdefgh123456?fm=jpg&q=60&w=3000",
         username: "Elena C.",
-        posttext: "No me gustó el ambiente, pero la comida era buena.",
-        postimg: "",
+        postText: "No me gustó el ambiente, pero la comida era buena.",
+        postImg: "",
         likes: 20,
     },
     {
         uid: 13,
         isEvent: false,
-        profileimg:
+        profileImg:
             "https://plus.unsplash.com/premium_photo-1681234567890-ijklmn123456?fm=jpg&q=60&w=3000",
         username: "Pablo Q.",
-        posttext: "Increíble experiencia. ¡Recomendado!",
-        postimg:
+        postText: "Increíble experiencia. ¡Recomendado!",
+        postImg:
             "https://plus.unsplash.com/premium_photo-1688901234567-ijklmn654321?fm=jpg&q=60&w=3000",
         likes: 130,
     },
     {
         uid: 14,
         isEvent: true,
+        isAttending: false,
         eventTitle: "Fiesta de cumpleaños en el parque",
         eventLocation: "Parque Central",
         eventDate: "2024-11-10",
@@ -154,6 +155,7 @@ export const posts = [
     {
         uid: 15,
         isEvent: true,
+        isAttending: true,
         eventTitle: "Taller de yoga al aire libre",
         eventLocation: "Jardín Botánico",
         eventDate: "2024-11-15",
@@ -167,6 +169,7 @@ export const posts = [
     {
         uid: 16,
         isEvent: true,
+        isAttending: true,
         eventTitle: "Concierto de música clásica",
         eventLocation: "Auditorio Municipal",
         eventDate: "2024-11-18",
@@ -180,6 +183,7 @@ export const posts = [
     {
         uid: 17,
         isEvent: true,
+        isAttending: false,
         eventTitle: "Mercado de productos orgánicos",
         eventLocation: "Plaza de la Ciudad",
         eventDate: "2024-11-22",
@@ -193,6 +197,7 @@ export const posts = [
     {
         uid: 18,
         isEvent: true,
+        isAttending: false,
         eventTitle: "Carrera de obstáculos",
         eventLocation: "Parque Acuático",
         eventDate: "2024-11-25",
@@ -206,6 +211,7 @@ export const posts = [
     {
         uid: 19,
         isEvent: true,
+        isAttending: true,
         eventTitle: "Cata de vinos",
         eventLocation: "Viñedo local",
         eventDate: "2024-11-29",
@@ -219,6 +225,7 @@ export const posts = [
     {
         uid: 20,
         isEvent: true,
+        isAttending: false,
         eventTitle: "Festival de cine al aire libre",
         eventLocation: "Parque de la Ciudad",
         eventDate: "2024-12-02-03",
