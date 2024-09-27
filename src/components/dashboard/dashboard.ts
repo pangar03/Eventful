e169f8511d552bd44854ad42edc70861900f29fb
Pangar03 <plopez0428@hotmail.com>
2024-09-27 09:39:33 -0500

    fix: migrate dashboard functionality to indexAbuelo.ts

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -14,50 +14,57 @@ class Dashboard extends HTMLElement {
     }
 
     connectedCallback() {
-        this.render();
+        console.log('hola');
+        
 
-        this.normalpost = posts.map((post) => {
-            if(!post.isEvent){
-                const postCard = this.ownerDocument.createElement('normal-post') as Post;
+        const normalPost = posts.filter((post) => !post.isEvent);
+        console.log('normalPost', normalPost);
 
+        const eventPost = posts.filter((post) => post.isEvent);
+        console.log('eventPost', eventPost);
+
+        normalPost.forEach((post) => {
+            const postCard = this.ownerDocument.createElement('normal-post') as Post;
                 postCard.setAttribute(PostAttribute.profileimg, post.profileImg || "");
                 postCard.setAttribute(PostAttribute.username, post.username || "");
                 postCard.setAttribute(PostAttribute.posttext, post.postText || "");
                 postCard.setAttribute(PostAttribute.postimg, post.postImg || "");
                 postCard.setAttribute(PostAttribute.likes, String(post.likes) || "");
-                return postCard;
-            }
-        }).filter((post): post is Post => post !== undefined);
+                console.log('post', postCard);
+                this.normalpost.push(postCard);
+        })
         
-        this.eventPost = posts.map((post) => {
-            if(post.isEvent){
-                const postCard = this.ownerDocument.createElement('normal-post') as EventPostCard;
+        eventPost.forEach((post) => {
+            const postCard = this.ownerDocument.createElement('event-post-card') as EventPostCard;
+            postCard.setAttribute(EventCardAttribute.image, post.eventImg || "");
+            postCard.setAttribute(EventCardAttribute.eventtitle, post.eventTitle || "");
+            postCard.setAttribute(EventCardAttribute.location, post.eventLocation || "");
+            postCard.setAttribute(EventCardAttribute.date, post.eventDate || "");
+            postCard.setAttribute(EventCardAttribute.description, post.description || "");
+            postCard.setAttribute(EventCardAttribute.likes, String(post.likes) || "");
+            postCard.setAttribute(EventCardAttribute.creator, post.creator || "");
+            postCard.setAttribute(EventCardAttribute.attendants, String(post.attendants) || "");
+            postCard.setAttribute(EventCardAttribute.maxattendants, String(post.maxAttendants) || "");
+            postCard.setAttribute(EventCardAttribute.isattending, String(post.isAttending) || "");
+            this.eventPost.push(postCard);
+        });
 
-                postCard.setAttribute(EventCardAttribute.image, post.eventImg || "");
-                postCard.setAttribute(EventCardAttribute.eventtitle, post.eventTitle || "");
-                postCard.setAttribute(EventCardAttribute.location, post.eventLocation || "");
-                postCard.setAttribute(EventCardAttribute.date, post.eventDate || "");
-                postCard.setAttribute(EventCardAttribute.description, post.description || "");
-                postCard.setAttribute(EventCardAttribute.likes, String(post.likes) || "");
-                postCard.setAttribute(EventCardAttribute.creator, post.creator || "");
-                postCard.setAttribute(EventCardAttribute.attendants, String(post.attendants) || "");
-                postCard.setAttribute(EventCardAttribute.maxattendants, String(post.maxAttendants) || "");
-                postCard.setAttribute(EventCardAttribute.isattending, String(post.isAttending) || "");
-                return postCard;
-            }
-        }).filter((post): post is EventPostCard => post !== undefined);
+        this.render();
         
-        this.initialRender();
     }
 
     render() {
+
         console.log("FLAG!!")
+        console.log('noemal post', this.normalpost);
+        
         if(this.shadowRoot){
             this.shadowRoot.innerHTML = `
                 <link rel="stylesheet" href="/src/components/dashboard/dashboard.css">
                 <section class="dashboard"></section>
             `;
         } 
+        this.initialRender();
     }
 
     initialRender() {
@@ -79,3 +86,4 @@ class Dashboard extends HTMLElement {
 
 customElements.define('dashboard-component', Dashboard);
 export default Dashboard;
+
--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -1,18 +1,52 @@
 import EventPageDetails, {Attribute as EventPageAttribute} from "./components/eventPageDetails/eventPageDetails";
 import EventPostCard, {Attribute as EventCardAttribute} from "./components/eventPostCard/eventPostCard";
 import Post, { Attribute as PostAttribute } from './components/normal-post/normal-post';
-import Dashboard from './components/dashboard/dashboard';
+
 
 import { posts } from './data/data';
 
 import * as components from './components/indexPadre';
+import Dashboard from "./components/dashboard/dashboard";
 
 class AppContainer extends HTMLElement {
     normalpost: Post[] = [];
+    eventPost: EventPostCard[] = [];
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+
+        const normalPost = posts.filter((post) => !post.isEvent);
+        console.log('normalPost', normalPost);
+
+        const eventPost = posts.filter((post) => post.isEvent);
+        console.log('eventPost', eventPost);
+
+        normalPost.forEach((post) => {
+            const postCard = this.ownerDocument.createElement('normal-post') as Post;
+                postCard.setAttribute(PostAttribute.profileimg, post.profileImg || "");
+                postCard.setAttribute(PostAttribute.username, post.username || "");
+                postCard.setAttribute(PostAttribute.posttext, post.postText || "");
+                postCard.setAttribute(PostAttribute.postimg, post.postImg || "");
+                postCard.setAttribute(PostAttribute.likes, String(post.likes) || "");
+                console.log('post', postCard);
+                this.normalpost.push(postCard);
+        })
+        
+        eventPost.forEach((post) => {
+            const postCard = this.ownerDocument.createElement('event-post-card') as EventPostCard;
+            postCard.setAttribute(EventCardAttribute.image, post.eventImg || "");
+            postCard.setAttribute(EventCardAttribute.eventtitle, post.eventTitle || "");
+            postCard.setAttribute(EventCardAttribute.location, post.eventLocation || "");
+            postCard.setAttribute(EventCardAttribute.date, post.eventDate || "");
+            postCard.setAttribute(EventCardAttribute.description, post.description || "");
+            postCard.setAttribute(EventCardAttribute.likes, String(post.likes) || "");
+            postCard.setAttribute(EventCardAttribute.creator, post.creator || "");
+            postCard.setAttribute(EventCardAttribute.attendants, String(post.attendants) || "");
+            postCard.setAttribute(EventCardAttribute.maxattendants, String(post.maxAttendants) || "");
+            postCard.setAttribute(EventCardAttribute.isattending, String(post.isAttending) || "");
+            this.eventPost.push(postCard);
+        });
     }
 
     connectedCallback() {
@@ -22,15 +56,27 @@ class AppContainer extends HTMLElement {
     render() {
         if(this.shadowRoot){
             this.shadowRoot.innerHTML = `
-                <link rel="stylesheet" href="/src/indexAbuelo.css">`;
+                <link rel="stylesheet" href="/src/indexAbuelo.css">
+                <h1>APP CONTAINER</h1>
+                <section class='dashboard'></section>`;
+                
         }
 
-        // Adding the dashboard component
-        const dashboard = this.ownerDocument.createElement('dashboard-component') as Dashboard;
-        console.log("FLAG!");
-        console.log(dashboard);
-        this.shadowRoot?.appendChild(dashboard);
+        if(this.shadowRoot){
+            const dashboard = this.shadowRoot.querySelector('.dashboard');
+            if(dashboard){
+                this.normalpost.forEach((post) => {
+                    dashboard.appendChild(post);
+                });
+                this.eventPost.forEach((post) => {
+                    dashboard.appendChild(post);
+                });
+            }
+        }
+
     }
 }
 
 customElements.define('app-container', AppContainer);
+
+
