f4960a7f8f93b9547e87c15f234f18f6ad1c7e34
Pangar03 <plopez0428@hotmail.com>
2024-10-28 17:38:56 -0500

    refac: adjust the actions type

--- src/types/store.ts
+++ src/types/store.ts
@@ -12,9 +12,7 @@ export type Observer = { render: () => void } & HTMLElement;
 export enum Actions {
     'NAVIGATE' = 'NAVIGATE',
     'GETPOSTS' = 'GETPOSTS',
-    'LIKEPOST' = 'LIKEPOST',
-    'CONFIRMATTENDANCE' = 'CONFIRMATTENDANCE',
-    'CANCELATTENDANCE' = 'CANCELATTENDANCE',
+    'OPENPOST' = 'OPENPOST',
 }
 
 export enum Screens {
