8b994753f7897756ff2f8c9d8f5fb1ef96813e18
Pangar03 <plopez0428@hotmail.com>
2024-10-28 15:21:54 -0500

    fix: create posts types

---
+++ src/types/post.ts
@@ -0,0 +1,24 @@
+export type NormalPost = {
+    uid: number,
+    isEvent: boolean,
+    profileImg: string, 
+    username: string,
+    postText: string,
+    postImg: string,
+    likes: number,
+    isLiked: boolean,
+};
+
+export type EventPost = {
+    uid: number,
+    isEvent: boolean,
+    isAttending: boolean,
+    eventTitle: string,
+    eventLocation: string,
+    eventDate: string,
+    description: string,
+    creator: string,
+    eventImg: string,
+    attendants: number,
+    maxAttendants: number,
+}
