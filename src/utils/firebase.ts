cccb55e880e33c78a7aa9998245b9e8058727f11
Pangar03 <plopez0428@hotmail.com>
2024-10-29 19:14:22 -0500

    fix: set up the firebase utility file

---
+++ src/utils/firebase.ts
@@ -0,0 +1,73 @@
+import { firebaseConfig } from "../../fbcg";
+
+let db: any;
+let auth: any;
+
+const getFirebaseInstance = async () => {
+    if(!db){
+        const { getFirestore } = await import("firebase/firestore");
+        const { initializeApp } = await import('firebase/app');
+		const { getAuth } = await import('firebase/auth');
+
+        const app = initializeApp(firebaseConfig);
+        db = getFirestore(app);
+        auth = getAuth(app);
+    }
+
+    return { db, auth };
+}
+
+
+export const getPosts = async () => {
+    try {
+		const { db } = await getFirebaseInstance();
+		const { collection, getDocs } = await import('firebase/firestore');
+
+		const where = collection(db, 'products');
+		const querySnapshot = await getDocs(where);
+		const data: any[] = [];
+
+		querySnapshot.forEach((doc) => {
+			data.push(doc.data());
+		});
+
+		return data;
+	} catch (error) {
+		console.error('Error getting documents', error);
+	}
+};
+
+export const registerUser = async (credentials: any) => {
+	try {
+		const { auth, db } = await getFirebaseInstance();
+		const { createUserWithEmailAndPassword } = await import('firebase/auth');
+		const { doc, setDoc } = await import('firebase/firestore');
+
+		const userCredential = await createUserWithEmailAndPassword(auth, credentials.email, credentials.password);
+
+		const where = doc(db, 'users', userCredential.user.uid);
+		const data = {
+            // ADD THE EXTRA DATA REQUIRED WHEN REGISTERING A USER
+		};
+
+		await setDoc(where, data);
+		return true;
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+};
+
+export const loginUser = async (email: string, password: string) => {
+	try {
+		const { auth } = await getFirebaseInstance();
+		const { signInWithEmailAndPassword } = await import('firebase/auth');
+
+		const userCredential = await signInWithEmailAndPassword(auth, email, password);
+		console.log(userCredential.user);
+		return true;
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+};
