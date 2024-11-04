f043715a3f56917273e31a17885c1c07405b36b3
Pangar03 <plopez0428@hotmail.com>
2024-11-04 16:24:27 -0500

    feat: add the base functionality for the create post form

--- src/components/createPostForm/createPostForm.ts
+++ src/components/createPostForm/createPostForm.ts
@@ -1,5 +1,10 @@
 import Styles from './createPostForm.css';
 
+import { dispatch } from '../../store';
+import { navigate } from '../../store/actions';
+import { Screens } from '../../types/store';
+import { addPost } from '../../utils/firebase';
+
 class CreatePostForm extends HTMLElement {
     constructor() {
         super();
@@ -12,10 +17,43 @@ class CreatePostForm extends HTMLElement {
 
     render() {
         if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+                <form>
+                    <label for="caption">Caption</label>
+                    <input type="text" id="caption" name="caption" required placeholder="Type here what you want to say to the world...">
+                    <div>
+                        <h3>Image</h3>
+                        <label for="image">Want to post an image as well? Select the option below to add a new picture.</label>
+                        <input type="text" id="image" name="image" placeholder="URL of the image"> <!-- CHANGE AS SOON AS STORAGE ISSUE IS RESOLVED -->
+                    </div>
+                    <button type="submit">Publish</button>
+                </form>
+            `;
+
+            const form = this.shadowRoot.querySelector('form')!;
+            form.addEventListener('submit', async (e) => {
+                e.preventDefault();
+
+                const caption = form.caption.value;
+                const image = form.image.value;
+
+                const post = {
+                    uid: new Date().getTime(),
+                    isEvent: false,
+                    profileImg: "https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331256_1280.png", // CHANGE WITH USER INFO
+                    username: "Default User", // CHANGE WITH USER INFO
+                    postText: caption,
+                    postImg: image,
+                    likes: 0,
+                }
+
+                await addPost(post);
+                dispatch(navigate(Screens.DASHBOARD));
+            });
 
             const css = this.ownerDocument.createElement('style');
             css.innerHTML = Styles;
-            this.shadowRoot.appendChild(css);
+            this.shadowRoot?.appendChild(css);
         }
     }
 }
