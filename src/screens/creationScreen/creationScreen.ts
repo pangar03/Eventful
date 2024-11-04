8c55a8eec31eb6acdd9bee8558b4d1987e86520c
Pangar03 <plopez0428@hotmail.com>
2024-11-04 14:46:05 -0500

    fix: add the base design for the creation screen

--- src/screens/creationScreen/creationScreen.ts
+++ src/screens/creationScreen/creationScreen.ts
@@ -1,9 +1,19 @@
+import CreatePostForm from '../../components/createPostForm/createPostForm';
+import '../../components/createPostForm/createPostForm';
+
+import CreateEventForm from '../../components/createEventForm/createEventForm';
+import '../../components/createEventForm/createEventForm';
+
+import { addObserver } from '../../store';
 import Styles from './creationScreen.css';
 
 class CreationScreen extends HTMLElement {
+    isEvent: boolean = false;
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        addObserver(this);
     }
 
     connectedCallback() {
@@ -12,7 +22,33 @@ class CreationScreen extends HTMLElement {
 
     render() {
         if(this.shadowRoot){
-            this.shadowRoot.innerHTML = ``;
+            this.shadowRoot.innerHTML = `
+                <section>
+                    <div>
+                        <h1>New Post</h1>
+                        <p> Want to post something new today? Want to create a new event? Select one of the options below</p>
+                        <div class="post-options">
+                            <button id="create-event">New Event</button>
+                            <button id="create-post">Regular Post</button>
+                        </div>
+                    </div>
+                    <div class="creation-form"></div>
+                </section>
+            `;
+
+            const formContainer = this.shadowRoot.querySelector('.creation-form');
+
+            if(formContainer){        
+                if(this.isEvent){
+                    formContainer.innerHTML = `
+                        <create-event-form></create-event-form>
+                    `;
+                } else {
+                    formContainer.innerHTML = `
+                        <create-post-form></create-post-form>
+                    `;
+                }
+            }
 
             const css = this.ownerDocument.createElement('style');
             css.innerHTML = Styles;
