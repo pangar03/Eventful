ed1d985ef553aba90beefd98af32dbfe6b9f273c
julianalinares88 <julianalinares1701@gmail.com>
2024-11-05 15:38:14 -0500

    style:typing errors

--- src/screens/loginScreen/loginScreen.ts
+++ src/screens/loginScreen/loginScreen.ts
@@ -26,7 +26,7 @@ class LoginScreen extends HTMLElement {
                             <h2 id="subtitle-bold">We missed you</h2>
                         </div>
                         
-                        <h2 id="subtitle-light">Hope on and discover what's new on your network.</h2>
+                        <h2 id="subtitle-light">Hop on and discover what's new on your network.</h2>
                     </div>
                     <login-form></login-form>
                 </section>
